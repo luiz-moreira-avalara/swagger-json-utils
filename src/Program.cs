58ce3e7c470e56fdbe1e35cf249367b18ffcbbaa
Luiz Moreira <luiz.moreira@avalara.com>
2017-12-07 15:49:40 -0200

    add path split method

--- src/Program.cs
+++ src/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+using System.Text;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
@@ -16,32 +17,67 @@ namespace Swagger.Json.Utils
                 Console.Read();
                 return;
             }
-            
-            var filepath = Path.Combine(Environment.CurrentDirectory, args.First());
 
-            TransformJson(filepath);
+            var filepath = Path.Combine(Environment.CurrentDirectory, args.First());
+            Directory.GetParent(filepath).CreateSubdirectory("paths");
+
+            TransformJson(filepath, Directory.GetParent(filepath));
 
             Console.WriteLine("Complete");
         }
 
-        private static void TransformJson(string filepath)
+        private static void TransformJson(string filepath, DirectoryInfo directory)
         {
+            void ChangeSummary(JObject node)
+            {
+                JToken summary = node["summary"];
+                JToken operationId = node["operationId"];
+                if (summary != null && summary.Type == JTokenType.String && operationId != null &&
+                    operationId.Type == JTokenType.String)
+                {
+                    node["summary"] = $"{operationId.Value<string>()} - {summary.Value<string>()}";
+                }
+            }
+
+            void SplitPaths(JObject node, string changedFilename)
+            {
+                var paths = node["paths"];
+
+                if (paths == null)
+                    return;
+
+                var baseFilePath= $@"{directory.FullName}\paths";
+                var childs = paths.Children<JProperty>();
+
+                foreach (var child in childs)
+                {
+                    foreach (var verb in child.Children<JObject>())
+                    {
+                        ChangeSummary(verb);
+                    }
+
+                    var filename = $"{ child.Name.Split("/").Last() }.json";
+                    var content = child.Value.ToString(Formatting.None).Replace("#", $@"../{changedFilename}#");
+                    File.WriteAllText(Path.Combine(baseFilePath, filename) , content, Encoding.UTF8);
+
+                    child.Value = JToken.Parse("{ \"$ref\": \"paths/" + filename +"\" }");
+                }
+            }
+
+
             using (var fileStream = File.OpenText(filepath))
             {
                 var jsonText = fileStream.ReadToEndAsync().Result;
                 var jsonObject = JObject.Parse(jsonText);
+                var path = filepath.Replace(".json", $"-changed.json");
+                var changedFileName = path.Split(@"\").Last();
                 WalkNode(jsonObject, node =>
                 {
-                    JToken summary = node["summary"];
-                    JToken operationId = node["operationId"];
-                    if (summary != null && summary.Type == JTokenType.String && operationId != null &&
-                        operationId.Type == JTokenType.String)
-                    {
-                        node["summary"] = $"{operationId.Value<string>()} - {summary.Value<string>()}";
-                    }
+                    ChangeSummary(node);
+                    SplitPaths(node, changedFileName);
                 });
 
-                using (StreamWriter outputFile = new StreamWriter(filepath.Replace(".json", $"-changed.json")))
+                using (StreamWriter outputFile = new StreamWriter(path))
                 {
                     outputFile.WriteLine(JsonConvert.SerializeObject(jsonObject, new JsonSerializerSettings
                     {
